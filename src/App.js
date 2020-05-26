59e3b3dfd3ba6a243e6ae26492f1c036faeb5683
Henrique <henriqueps_98@hotmail.com>
2020-05-26 19:02:51 -0300

    Supporting Mobile Devices

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 // in src/App.js
 import React from "react";
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
+import { PostList, PostEdit, PostCreate } from "./posts";
+import { UserList } from "./users";
 import jsonServerProvider from "ra-data-json-server";
+import PostIcon from "@material-ui/icons/Book";
+import UserIcon from "@material-ui/icons/Group";
+import Dashboard from "./Dashboard";
+import authProvider from "./authProvider";
 
 const dataProvider = jsonServerProvider("https://jsonplaceholder.typicode.com");
 const App = () => (
-  <Admin dataProvider={dataProvider}>
-    <Resource name="users" list={ListGuesser} />
+  <Admin
+    dashboard={Dashboard}
+    authProvider={authProvider}
+    dataProvider={dataProvider}
+  >
+    <Resource
+      name="posts"
+      list={PostList}
+      edit={PostEdit}
+      create={PostCreate}
+      icon={PostIcon}
+    />
+    <Resource name="users" list={UserList} icon={UserIcon} />
   </Admin>
 );
 
